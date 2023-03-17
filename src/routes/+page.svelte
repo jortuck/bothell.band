1e4db32362202c27e55d960018294ab17f0e14ca
DriedSponge <jordan@driedsponge.net>
2023-03-16 20:35:29 -0700

    Rebranded to bothell jazz

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,92 +3,92 @@
 </script>
 
 <svelte:head>
-	<title>Bothell Band</title>
+	<title>Bothell Jazz</title>
 	<meta
 		name="description"
-		content="Discover the latest news and events from Bothell High School's band! Get updates on upcoming performances, buy tickets, and learn how you can support the band's fundraising efforts."
+		content="Discover the latest news and events from Bothell High School's Jazz Band! Get updates on upcoming performances, buy tickets, and learn how you can support the band's fundraising efforts."
 	/>
 	<meta
 		property="og:title"
-		content="Bothell Band"
+		content="Bothell Jazz"
 	/>
 </svelte:head>
 <h1
 	data-aos="fade-down"
-	class="text-center text-white text-6xl md:text-7xl lg:text-8xl  bebasneue my-12 align-middle w-full"
+	class="jazztext my-12 w-full text-center align-middle text-6xl text-white md:text-7xl lg:text-7xl"
 >
-	<span class="bg-bothellblue px-2">BOTHELL</span><span class="text-bothellblue bg-white px-2"
-		>BAND</span
+	<span class="bg-bothellblue px-2">BOTHELL</span><span class="bg-white px-2 text-bothellblue"
+		>JAZZ</span
 	>
 </h1>
 <section class="space-y-10">
 	<h2
-		class="bebasneue text-center text-white text-4xl"
+		class="bebasneue text-center text-4xl text-white"
 		data-aos="fade-down"
 	>
 		Upcoming Events...
 	</h2>
-	<div class="flex flex-col md:flex-row items-center justify-center space-y-10 md:space-y-0">
+	<div class="flex flex-col items-center justify-center space-y-10 md:flex-row md:space-y-0">
 		<div
-			class="mx-4 shadow-2xl max-w-2xl"
+			class="mx-4 max-w-2xl shadow-2xl"
 			data-aos="fade-up"
 		>
 			<div class="md:h-80">
 				<img
-					class="md:object-fill md:h-full"
+					class="md:h-full md:object-fill"
 					alt="hot java cool jazz logo"
 					src="/hotjava.jpg"
 				/>
 			</div>
 
 			<div class="bg-bgsecondary py-2 px-1">
-				<h3 class="bebasneue text-center text-white text-2xl">
+				<h3 class="bebasneue text-center text-2xl text-white">
 					Starbucks 26th Annual Hot Java Cool Jazz
 				</h3>
-				<h3 class="bebasneue text-center text-white text-2xl">
+				<h3 class="bebasneue text-center text-2xl text-white">
 					March 31 2023 | The Paramount Theatre
 				</h3>
 			</div>
 			<div class="flex flex-row">
 				<a
 					rel="noreferrer"
-					class="bebasneue w-full bg-gray-500 text-white block text-center text-3xl py-3 hover:bg-gray-400"
+					class="bebasneue block w-full bg-gray-500 py-3 text-center text-3xl text-white hover:bg-gray-400"
 					href="https://www.stgpresents.org/calendar/13775/starbucks-26th-annual-hot-java-cool-jazz"
 					target="_blank">Learn More</a
 				>
 				<a
 					rel="noreferrer"
-					class="bebasneue w-full bg-yellow-600 text-white block text-center text-3xl py-3 hover:bg-yellow-500 "
+					class="bebasneue block w-full bg-yellow-600 py-3 text-center text-3xl text-white hover:bg-yellow-500 "
 					href="https://my.cheddarup.com/c/bothellmusicboosters/item/4677147"
 					target="_blank">Purchase Tickets</a
 				>
 			</div>
 		</div>
 		<div
-			class="mx-4 shadow-2xl max-w-2xl"
+			class="mx-4 max-w-2xl shadow-2xl"
 			data-aos="fade-up"
 		>
 			<div class="md:h-80">
 				<img
 					alt="Essentially Ellington Logo"
-					class="md:object-fill md:h-full"
+					class="md:h-full md:object-fill"
 					src="/ellington.jpg"
 				/>
 			</div>
 			<div class="bg-bgsecondary py-2 px-1">
-				<h3 class="bebasneue text-center text-white text-2xl">Essentially Ellington</h3>
-				<h3 class="bebasneue text-center text-white text-2xl">May 11th-13th | New York City</h3>
+				<h3 class="bebasneue text-center text-2xl text-white">Essentially Ellington</h3>
+				<h3 class="bebasneue text-center text-2xl text-white">May 11th-13th | New York City</h3>
 			</div>
 			<div class="flex flex-row">
 				<a
 					rel="noreferrer"
-					class="bebasneue w-full bg-gray-500 text-white block text-center text-3xl py-3 hover:bg-gray-400 "
+					class="bebasneue block w-full bg-gray-500 py-3 text-center text-3xl text-white hover:bg-gray-400 "
 					href="https://2023.jazz.org/essentially-ellington"
 					target="_blank">Learn More</a
 				>
 				<a
 					rel="noreferrer"
-					class="bebasneue w-full bg-yellow-600 text-white block text-center text-3xl py-3 hover:bg-yellow-500 "
+					class="bebasneue block w-full bg-yellow-600 py-3 text-center text-3xl text-white hover:bg-yellow-500 "
 					href="https://my.cheddarup.com/c/bothellmusicboosters/item/4724968"
 					target="_blank">Donate</a
 				>
@@ -98,13 +98,13 @@
 </section>
 <section class="mt-10 space-y-10">
 	<h2
-		class="bebasneue text-center text-white text-4xl"
+		class="bebasneue text-center text-4xl text-white"
 		data-aos="fade-up"
 	>
 		Additional Resources
 	</h2>
-	<div class="w-full flex items-center flex-col">
-		<div class="text-center space-y-4  flex flex-col w-full items-center items-stretch max-w-2xl">
+	<div class="flex w-full flex-col items-center">
+		<div class="flex w-full  max-w-2xl flex-col items-center items-stretch space-y-4 text-center">
 			<Buttonlink url="https://www.bothellmusicboosters.org/">Bothell Music Boosters</Buttonlink>
 			<Buttonlink url="https://www.facebook.com/bothellmusicboosters"
 				>Bothell Music Boosters Facebook</Buttonlink
