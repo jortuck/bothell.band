d25e31fcb4356a5535f4097ae42184d8d06389cb
DriedSponge <jordan@driedsponge.net>
2023-04-18 22:47:43 -0700

    Went back to cloudflare becuase it supports 404

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-//import adapter from "@sveltejs/adapter-cloudflare";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
+//import adapter from "@sveltejs/adapter-static";
 
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
