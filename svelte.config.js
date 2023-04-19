939b353b15e9cc3823ba51a435377cc8ed0c5a2b
DriedSponge <jordan@driedsponge.net>
2023-04-18 22:44:46 -0700

    Move to adapter static to try to improve load times

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+//import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
+
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
