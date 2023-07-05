47b69f95aeedc7d1955e21b0dbf210d236da4c43
jordan <jordan@driedsponge.net>
2023-07-04 19:24:16 -0700

    Update to svelte 4

--- src/lib/Buttonlink.svelte
+++ src/lib/Buttonlink.svelte
@@ -8,7 +8,7 @@
 <a
 	role="button"
 	href={url}
-	target:target
+	{target}
 	rel={refer ? null : "noreferrer"}
 	class="bebasneue mx-3 bg-bothellblue p-3 align-middle text-3xl text-white shadow-2xl transition-all duration-300 ease-in-out hover:bg-white hover:text-bothellblue active:translate-y-1 md:text-4xl"
 >
